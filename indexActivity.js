1345af146fa0e8089192263c8681b09ca201d545
AngeloJamesRomerosa <125255849+AngeloJamesRomerosa@users.noreply.github.com>
2023-03-20 17:10:04 +0800

    first commit

---
+++ indexActivity.js
@@ -0,0 +1,21 @@
+console.log('Hello, Welcome to De La Salle University!');
+
+let firstName = "Angelo James";
+let lastName = "Romerosa";
+let age = 21;
+let hobbies = ["Watching Anime","Reading Manga","Cycling","Watching Documentaries","Reading Novels"];
+let homeAddress = {
+	houseNumber: "Blk 9 Lot 18",
+	street: null,
+	subdivision: "Richlane",
+	barangay: "Anabu-2E",
+	city: "Imus",
+	province: "Cavite",
+	zipcode: 4103
+};
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hobbies);
+console.log(homeAddress);
